eb8240b19c1b8124a739569c11bb7af696057eb5
John Triantafyllakis <john.3fyl@gmail.com>
2023-12-24 14:48:35 +0200

    Implemented find order by order id

--- order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/adapter/OrderRepositoryImpl.java
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/adapter/OrderRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.food.ordering.system.order.service.dataaccess.order.adapter;
 
+import com.food.ordering.system.domain.valueobject.OrderId;
 import com.food.ordering.system.order.service.dataaccess.order.mapper.OrderDataAccessMapper;
 import com.food.ordering.system.order.service.dataaccess.order.repository.OrderJpaRepository;
 import com.food.ordering.system.order.service.domain.entity.Order;
@@ -8,9 +9,10 @@ import com.food.ordering.system.order.service.domain.valueobject.TrackingId;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
+
 @Component
-public class OrderRepositoryImpl  implements OrderRepository {
-    private  final OrderJpaRepository orderJpaRepository;
+public class OrderRepositoryImpl implements OrderRepository {
+    private final OrderJpaRepository orderJpaRepository;
     private final OrderDataAccessMapper orderDataAccessMapper;
 
     public OrderRepositoryImpl(OrderJpaRepository orderJpaRepository, OrderDataAccessMapper orderDataAccessMapper) {
@@ -32,5 +34,10 @@ public class OrderRepositoryImpl  implements OrderRepository {
     public Optional<Order> findByTrackId(TrackingId trackingId) {
         return orderJpaRepository.findByTrackingId(trackingId.getValue())
                 .map(orderDataAccessMapper::orderEntityToOrder);
+    }
+
+    @Override
+    public Optional<Order> findById(OrderId orderId) {
+        return orderJpaRepository.findById(orderId.getValue()).map(orderDataAccessMapper::orderEntityToOrder);
     }
 }
--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/repository/OrderRepository.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/repository/OrderRepository.java
@@ -1,12 +1,15 @@
 package com.food.ordering.system.order.service.domain.ports.output.repository;
 
-import java.util.Optional;
-
+import com.food.ordering.system.domain.valueobject.OrderId;
 import com.food.ordering.system.order.service.domain.entity.Order;
 import com.food.ordering.system.order.service.domain.valueobject.TrackingId;
 
-public interface OrderRepository {
-	Order save(Order order);
+import java.util.Optional;
 
-	Optional<Order> findByTrackId(TrackingId trackingId);
+public interface OrderRepository {
+    Order save(Order order);
+
+    Optional<Order> findByTrackId(TrackingId trackingId);
+
+    Optional<Order> findById(OrderId orderId);
 }
