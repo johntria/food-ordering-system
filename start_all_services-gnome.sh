f14f5d9d23fb8e5f2f94409daf90f33a170dfe13
John Triantafyllakis <john.3fyl@gmail.com>
2023-12-26 22:59:58 +0200

    Updated bash script in order to kill all opened terminals at exit

--- start_all_services-gnome.sh
+++ start_all_services-gnome.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+# Array to store gnome-terminal PIDs
+gnome_pids=()
+
 # Function to clean up and close all windows
 cleanup() {
     echo "Cleaning up and closing all windows..."
-
+    
     # Stop all running containers
     docker compose -f common.yml -f postgres.yml down
     docker compose -f common.yml -f zookeeper.yml down
     docker compose -f common.yml -f kafka_cluster.yml down
     docker compose -f common.yml -f init_kafka.yml down
-
-    # Kill all gnome-terminal processes
-    pkill gnome-terminal
-
+    
+    # Kill all gnome-terminal processes opened by the script
+    for pid in "${gnome_pids[@]}"; do
+        kill "$pid"
+    done
+    
     exit 1
 }
 
@@ -22,23 +27,30 @@ trap cleanup INT
 cd infrastructure/docker-compose
 
 # Start Postgres SQL
-gnome-terminal --title="postgres" -- bash -c "docker compose -f common.yml -f postgres.yml up --build; exec bash"
+gnome-terminal --title="postgres" -- bash -c "docker compose -f common.yml -f postgres.yml up --build; exec bash" &
+gnome_pids+=($!)
+
 echo "Postgres started successfully!"
 
 # Start Zookeeper
-gnome-terminal --title="zookeeper" -- bash -c "docker compose -f common.yml -f zookeeper.yml up --build; exec bash"
+gnome-terminal --title="zookeeper" -- bash -c "docker compose -f common.yml -f zookeeper.yml up --build; exec bash" &
+gnome_pids+=($!)
+
 echo "Zookeeper started successfully!"
 
 # Add sleep to allow Zookeeper to start successfully.
-##TODO add depends on with include yml file .
 sleep 30
 
-# Start Kafka Clusters
-gnome-terminal --title="kafka_clusters" -- bash -c "docker compose -f common.yml -f kafka_cluster.yml up --build; exec bash"
-echo "Kafka Clusters started successfully!"
+# Start Kafka Cluster
+gnome-terminal --title="kafka_cluster" -- bash -c "docker compose -f common.yml -f kafka_cluster.yml up --build; exec bash" &
+gnome_pids+=($!)
+
+echo "Kafka Cluster started successfully!"
 
 # Start init_kafka
-gnome-terminal --title="init_kafka" -- bash -c "docker compose -f common.yml -f init_kafka.yml up --build; exec bash"
+gnome-terminal --title="init_kafka" -- bash -c "docker compose -f common.yml -f init_kafka.yml up --build; exec bash" &
+gnome_pids+=($!)
+
 echo "Initialization kafka  started successfully!"
 
 # Print all running containers
