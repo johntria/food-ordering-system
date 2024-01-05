6a88e3f35f4731513b7a1095aa3a89bb19d4ff4c
John Triantafyllakis <john.3fyl@gmail.com>
2024-01-05 19:30:21 +0200

    Refactoring Order domain layer: Adding Outbox scheduler WIP

---
+++ infrastructure/saga/src/main/java/com/food/ordering/system/saga/order/SagaConstants.java
@@ -0,0 +1,8 @@
+package com.food.ordering.system.saga.order;
+
+public class SagaConstants {
+    public SagaConstants() {
+    }
+
+    public static final String ORDER_SAGA_NAME = "OrderProcessingSaga";
+}
