05f7b8c562c64dde163d3fe68b8662ec8c14d99b
John Triantafyllakis <john.3fyl@gmail.com>
2024-01-21 21:05:27 +0200

    Deleted old implementation of fire event

--- payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/PaymentRequestMessageListenerImpl.java
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/PaymentRequestMessageListenerImpl.java
@@ -30,6 +30,5 @@ public class PaymentRequestMessageListenerImpl implements PaymentRequestMessageL
 
     private void fireEvent(PaymentEvent paymentEvent) {
         log.info("Publishing payment event with payment id: {} and order id: {}", paymentEvent.getPayment().getId().getValue(), paymentEvent.getPayment().getOrderId().getValue());
-        paymentEvent.fire();
     }
 }
